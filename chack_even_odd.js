fff49df492723ebe923f00902e2343ffd4ca4ae8
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2025-01-07 02:17:06 +0600

    chack even odd number

---
+++ chack_even_odd.js
@@ -0,0 +1,8 @@
+const x = prompt("Enter a number : ");
+const number = parseInt(x);
+
+if (number % 2 == 0) {
+    print(`${number} is an even number`)
+} else {
+    print(`${number} is odd number`);
+}
