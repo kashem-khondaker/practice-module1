a5c70e78a4745a67b6b2e1bb5b0c56ddcbdd60c3
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2025-01-07 02:26:02 +0600

    is devided by 3 and 5

--- chack_even_odd.js
+++ chack_even_odd.js
@@ -1,6 +1,8 @@
 const x = prompt("Enter a number : ");
 const number = parseInt(x);
 
+var y = 10;
+
 if (number % 2 == 0) {
     print(`${number} is an even number`)
 } else {
---
+++ is_devided3and5.js
@@ -0,0 +1,7 @@
+var x = 15;
+
+if (x%5==0 && x%3 == 0) {
+    console.log("Yes");
+} else{
+    console.log("No");
+};
